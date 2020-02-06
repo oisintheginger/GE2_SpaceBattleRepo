74d6966bbba6aeebbb0ee89d5a60220735508940
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2020-02-06 23:55:38 +0000

    Fixed Some Things

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class ManagerScript : MonoBehaviour
 {
-    [SerializeField] int generationDistance, gap;
+    [SerializeField] int generationDistance, gap, avoidanceVariable=1;
     public List<GameObject> obstacleObjects;
-    public List<Vector3> viablePoints;
+    public List<Vector3> viablePoints, nonViablePoints;
+    Dictionary<string, Vector3> pointsDictionary;
 
     private void Awake()
     {
-        for(int x = 0; x<generationDistance; x+=gap)
+        obstacleObjects.Clear();
+        foreach(GameObject obstacle in GameObject.FindGameObjectsWithTag("obstacle"))
+        {
+            obstacleObjects.Add(obstacle);
+        }
+        for (int x = 0; x<generationDistance; x+=gap)
         {
             for(int z =0; z<generationDistance; z+=gap)
             {
-                Debug.Log(x);
                 Vector3 newPoint1 = new Vector3(x,0,z);
                 Vector3 newPoint2 = new Vector3(-x, 0, z);
                 Vector3 newPoint3 = new Vector3(x, 0, -z);
                 Vector3 newPoint4 = new Vector3(-x, 0, -z);
+                viablePoints.Add(newPoint1);
+                viablePoints.Add(newPoint2);
+                viablePoints.Add(newPoint3);
+                viablePoints.Add(newPoint4);
                 CheckPositions(newPoint1);
                 CheckPositions(newPoint2);
                 CheckPositions(newPoint3);
@@ -26,35 +35,31 @@ public class ManagerScript : MonoBehaviour
 
             }
         }
+        
     }
 
     void CheckPositions(Vector3 checkingPoints)
     {
         foreach(GameObject obstacle in obstacleObjects)
         {
-            if(Vector3.Distance(new Vector3(obstacle.transform.position.x, 0, obstacle.transform.position.z), checkingPoints)> obstacle.transform.localScale.x)
+            if(Vector3.Distance(new Vector3(obstacle.transform.position.x, 0, obstacle.transform.position.z), checkingPoints)< obstacle.transform.localScale.x * avoidanceVariable)
             {
-                viablePoints.Add(checkingPoints);
+                Debug.Log("Distance Checked");
+                viablePoints.Remove(checkingPoints);
+                nonViablePoints.Add(checkingPoints);
+            }
+            else
+            {
+                Debug.Log(checkingPoints + "This was reject");
             }
         }
         
     }
     private void OnDrawGizmos()
     {
-        foreach(Vector3 point in viablePoints)
+        foreach(Vector3 point in nonViablePoints)
         {
-            //Gizmos.DrawWireSphere(point, 1f);
+            Gizmos.DrawWireSphere(point, 1f);
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
